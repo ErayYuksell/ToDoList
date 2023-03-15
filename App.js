28dee49e4ed81852418198df4c19f3a67059e0d6
Eray Yüksel <108815807+ErayYuksell@users.noreply.github.com>
2023-03-15 17:24:11 +0300

    Section 2

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Task from "./src/components/Task";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss(); //task eklemesi yaptıktan sonra klavye kendiliğinden küçülür
+    setTaskItems([...taskItems, task]); //her bir taski öncekilerin üstüne ekleyerek bir task dizisi şeklinde yazdırmama yarıyor
+    setTask(null);
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
       {/* Today's task */}
@@ -10,10 +35,35 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
         <View style={styles.items}>
           {/* This is where the tasks will go */}
-          <Task text="Task 1"></Task>
-          <Task text="Task 2"></Task>
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item}></Task>
+              </TouchableOpacity>
+            );
+          })}
+          {/* <Task text="Task 1"></Task>
+          <Task text="Task 2"></Task> */}
         </View>
       </View>
+      {/* bu componenti kullanmamızın amacı bir şey yazmak için tıkladığımda klavye ile birlikte bu barın yükselmesi için */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Write a Task"
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        ></TextInput>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -29,7 +79,33 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  items: {marginTop: 30,},
+  items: { marginTop: 30 },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    padding: 15,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    borderWidth: 1,
+    borderColor: "#C0C0C0",
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#C0C0C0",
+  },
 });
